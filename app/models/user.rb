2ce3368ed0d10ede19443a835e30f1eee6b71b1e
Fig77 <fig.igle@gmail.com>
2020-03-30 19:49:14 -0300

    Added basic relation with votes model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :articles, foreign_key: :author_id
+  has_many :votes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_201813) do
+ActiveRecord::Schema.define(version: 2020_03_30_212017) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "author_id"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2020_03_30_201813) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "article_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
