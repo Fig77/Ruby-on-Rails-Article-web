3c3cb43cb53b4c82330f672d36bbcd32500c3d80
Fig77 <fig.igle@gmail.com>
2020-03-30 18:16:55 -0300

    added article-user relation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,3 @@
 class Article < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 end
--- db/migrate/20200330200753_create_articles.rb
+++ db/migrate/20200330200753_create_articles.rb
@@ -1,13 +1,12 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      t.integer :author_id
+      t.integer :author_id, references: :users, foreign_key: { to_table: :users }
       t.string :title
       t.string :text
       t.string :image
 
       t.timestamps
     end
-    add_foreign_key :articles, :users, column: :author_id
   end
 end
