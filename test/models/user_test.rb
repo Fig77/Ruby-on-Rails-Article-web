7e5e6e8828e08ee388ab2a629ab7a443b70142d8
Fig77 <fig.igle@gmail.com>
2020-03-31 19:52:55 -0300

    added user in fixture, started writing test cases for it

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+  	@u1 = users(:one)
+  end
+
+  test 'User should have a Collection of Associations' do
+  	col = 'Is not a collection'
+    assert_kind_of(@u1.articles, Association::CollectionProxy.new, col)
+  end
 end
