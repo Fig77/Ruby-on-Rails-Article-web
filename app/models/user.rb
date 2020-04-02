404e234843178515b15603f957873bca308071bc
Fig77 <fig.igle@gmail.com>
2020-04-02 00:23:33 -0300

    Fix minor bugs for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  attr_accessor :username
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
