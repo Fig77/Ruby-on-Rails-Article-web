025bba17bd79af22901dce7f1da8d2c4aaa754ff
Fig77 <fig.igle@gmail.com>
2020-04-01 23:46:47 -0300

    User can now login with username. Username added to user table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessor :username
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :articles, foreign_key: :author_id
