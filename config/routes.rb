c4dc094e939688dbebba95fbe4c17db0dfcf5e4b
Fig77 <fig.igle@gmail.com>
2020-04-06 14:35:00 -0300

    Added basice /new  - /show style

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :articles, only:[:new, :create, :show]
+  root 'articles#index'
   get 'index', to: 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
