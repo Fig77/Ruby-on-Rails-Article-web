b3f71e38001fd50265a4bbe22bc11b34c65a3cca
Fig77 <fig.igle@gmail.com>
2020-04-10 17:52:53 -0300

    Rubocop errors

--- app/models/article_category.rb
+++ app/models/article_category.rb
@@ -1,23 +1,20 @@
 class ArticleCategory < ApplicationRecord
   belongs_to :category
   belongs_to :article
-  order_group = "SELECT * FROM 
+  order_group = "SELECT * FROM
                 (SELECT * FROM article_categories ORDER BY created_at DESC)
-  				      AS sub GROUP BY category_id"      
-  scope :new_all, -> {
-  	self.connection.execute(order_group) 
+  				      AS sub GROUP BY sub.category_id"
+  scope :new_all, lambda {
+    connection.execute(order_group)
   }
   def self.getArray
     arrayHash = new_all
     hashArray = []
-    arrayHash.each { 
-     |x| 
+    arrayHash.each do |x|
       hashArray.push(x['article_id'])
-    }
-    where(id: hashArray).includes({article: [:image_attachment, :author]}, :category)
+    end
+    where(id: hashArray).includes({ article: %i[image_attachment author] }, :category)
   end
-
- 
 
   scope :ordered_by_most_recent, -> { order(created_at: :desc).group_by(:category_id) }
 end
