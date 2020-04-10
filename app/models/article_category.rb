5a7c0ab3a7b23b5a4e564bd6c6ca0081b03b9dd8
Fig77 <fig.igle@gmail.com>
2020-04-10 18:58:34 -0300

    Testing quick patch to avoid switching to postresql on development at this point, due to being a pain to make it run for some reason

--- app/models/article_category.rb
+++ app/models/article_category.rb
@@ -1,20 +1,28 @@
 class ArticleCategory < ApplicationRecord
   belongs_to :category
   belongs_to :article
-  order_group = "SELECT * FROM
-                (SELECT * FROM article_categories ORDER BY created_at DESC)
-  				      AS sub GROUP BY sub.category_id"
-  scope :new_all, lambda {
-    connection.execute(order_group)
+  unless Rails.env.production?
+    order_group = "SELECT *  FROM 
+                  (SELECT * FROM article_categories ORDER BY created_at DESC)
+    				      AS sub GROUP BY category_id" 
+  else
+    order_group = "SELECT DISTINCT ON (category_id) *
+                   FROM article_categories
+                   OREDER BY created_at"
+  end
+  scope :new_all, -> {
+  	self.connection.execute(order_group) 
   }
   def self.getArray
     arrayHash = new_all
     hashArray = []
-    arrayHash.each do |x|
+    arrayHash.each { 
+     |x| 
       hashArray.push(x['article_id'])
-    end
-    where(id: hashArray).includes({ article: %i[image_attachment author] }, :category)
+    }
+    where(id: hashArray).includes({article: [:image_attachment, :author]}, :category)
   end
 
+ 
   scope :ordered_by_most_recent, -> { order(created_at: :desc).group_by(:category_id) }
 end
