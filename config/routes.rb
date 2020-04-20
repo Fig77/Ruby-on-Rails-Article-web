e5b6d17153d2368bf2db5fdb06458a67226a081e
Fig77 <fig.igle@gmail.com>
2020-04-20 11:44:44 -0300

    Fixed route syntax

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :articles, only:[:new, :create]
   root 'articles#index'
-  get 'articles/:id', to: 'articles#show'
+  get 'article/:id', to: 'articles#show'
   get 'index', to: 'articles#index'
   put 'articles/:id', to: 'votes#update'
   get 'category/:id', to: 'categories#show', :as => :category
