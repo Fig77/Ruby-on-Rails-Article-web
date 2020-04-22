6ba03a75f1629845a0203a5d916b7ebcee42885d
Fig77 <fig.igle@gmail.com>
2020-04-22 16:02:08 -0300

    Rubocop errors fixed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,7 +7,7 @@ class CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:category).permit()
+    params.require(:category).permit
     # Still don't know what to permit since I am just showing them.
     # If they pass another id, then 404 should be shown.
   end
