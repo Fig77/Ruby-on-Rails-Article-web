c9d1ffe0ba9592527e4f6717270a8aa750adb066
Fig77 <fig.igle@gmail.com>
2020-04-22 18:51:40 -0300

    Added model validation for username since I honestly dont remember if it gets validated through devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :articles, foreign_key: :author_id
   has_many :votes
+
+  validates :username, length: { minimum: 3 }, presence: true, 
+  			uniqueness: true
 end
